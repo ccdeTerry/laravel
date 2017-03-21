25e9537b1d01bf837d93eee5d94d991b654c60a2
zhangtairui <ztr2012dl@163.com>
2017-03-21 17:33:32 +0800

    Initial commit

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,12 @@
+<?php
+namespace  App\Http\Controller;
+
+use App\Http\Controller;
+
+class UserController extends Controller{
+    
+    public  function  getUser(){
+        echo  '嘟嘟嘟嘟';
+    }
+    
+}
