19408891b7f0d64dfe25f1fb12e7ba61ae06257d
zhangxiaorui <ztr2012dl@163.com>
2017-03-27 23:13:12 +0800

    user

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class SessionController extends Controller
+{
+    //
+    
+    public   function  create(){
+        return view('session.create');
+    }
+
+    public function stort(){
+        
+    }
+ }
